8b23656a1fb986a9446f036337d62ac2dbb2abe7
Dong-eun Shin <7017410@naver.com>
2022-01-23 00:16:21 +0900

    Initial Commit

---
+++ MovieFinderApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFinderApp2
+//
+//  Created by dong eun shin on 2022/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
